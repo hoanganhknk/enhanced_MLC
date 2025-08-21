f8d47dd3a6952ff3292b499f38b132a227021e40
hoanganhknk <hoanganhnguyen.knk@gmail.com>
2025-08-21 15:05:56 +0700

    21/8

--- ebomlc.py
+++ ebomlc.py
@@ -63,27 +63,23 @@ def step_ebomlc(main_net, main_opt, hard_loss_f,
         loss_s = (loss_s * bs1 + loss_s2 * bs2) / (bs1 + bs2)
 
     gradient_g_mainparam = torch.autograd.grad(loss_s, main_net.parameters(), create_graph=True)
-    gradient_g_metaparam = torch.autograd.grad(loss_s, meta_net.parameters(), create_graph=True)
+
     
     f_params_new = update_params(main_net.parameters(), gradient_g_mainparam, eta, main_opt, args, return_s=False)
     for i, param in enumerate(main_net.parameters()):
         param.data = f_params_new[i]
     logit_s, x_s_h = main_net(data_s, return_h=True)
     pseudo_target_s = meta_net(x_s_h.detach(), target_s)
-    loss_s = soft_loss_f(logit_s, pseudo_target_s)
+    loss_s_new = soft_loss_f(logit_s, pseudo_target_s)
     if data_c is not None:
         bs1 = target_s.size(0)
         bs2 = target_c.size(0)
         logit_c = main_net(data_c)
         loss_s2 = hard_loss_f(logit_c, target_c)
-        loss_s = (loss_s * bs1 + loss_s2 * bs2) / (bs1 + bs2)
-    grad_g_mainparam_new = list(torch.autograd.grad(loss_s, main_net.parameters(), create_graph=True))
-    grad_g_metaparam_new = list(torch.autograd.grad(loss_s, meta_net.parameters(), create_graph=True))
+        loss_s_new = (loss_s * bs1 + loss_s2 * bs2) / (bs1 + bs2)
+    grad_g_mainparam_new = list(torch.autograd.grad(loss_s - loss_s_new, main_net.parameters(), create_graph=True))
+    grad_g_metaparam_new = list(torch.autograd.grad(loss_s - loss_s_new, meta_net.parameters(), create_graph=True))
     
-    for i in range(len(grad_g_mainparam_new)):
-        grad_g_mainparam_new[i] = gradient_g_mainparam[i] - grad_g_mainparam_new[i]
-    for i in range(len(grad_g_metaparam_new)):
-        grad_g_metaparam_new[i] = gradient_g_metaparam[i] - grad_g_metaparam_new[i]
     dq = torch.cat([grad_g_mainparam_new[i].view(-1) for i in range(len(grad_g_mainparam_new))]
                     + [grad_g_metaparam_new[i].view(-1) for i in range(len(grad_g_metaparam_new))])
     d_wq = torch.cat([grad_g_mainparam_new[i].view(-1) for i in range(len(grad_g_mainparam_new))])
